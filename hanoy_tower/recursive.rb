b0d8b4ea3906e496ee5bd9f41c533c8513617ac7
Irostovsky <ivan.rostovsky@gmail.com>
2009-08-31 20:08:50 +0000

    Add Tower

--- hanoy_tower/recursive.rb
+++ hanoy_tower/recursive.rb
@@ -1,7 +1,45 @@
 # Here we consider recursive solution of hanoy tower task
 
-def move(params=[3, 'src', 'dst', 'add'])
-  n, src, dst, add = params;
+class Tower
 
+  def initialize(count = 3)
+    @count = count
+    @src, @dst,  @add = [], [], []
+    (1..count).each do |i|
+      s = ''
+      i.times{ s+= '*' }
+      @src << s
+    end
+
+  end
+
+  def result
+    move(@count, @src, @dst, @add)
+    p @src
+    p @dst
+    p @add
+
+  end
+
+  private
+
+  def move(count, from, to, add)
+    if count == 1
+      single_move(from, to)
+    else
+      move count-1, from, add, to
+      move 1, from, to, add
+      move count-1, add, to, from
+    end
+
+  end
+
+
+  def single_move( from, to)
+    to.unshift(from.shift)
+  end
 
 end
+
+Tower.new(4).result
+
